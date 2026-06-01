7df48f5981570fc5d28feadf6d0ad823b48dc6d9
Dmitriy <dmitriy.svitkov2@mail.ru>
2026-06-01 20:12:26 +0300

    Update

--- PPOIS/lab_2/bullshit.py
+++ PPOIS/lab_2/bullshit.py
@@ -1,14 +1,1 @@
-
-
-def append_milo(lst: list = []):
-    lst.append("milo xui")
-    print("значение списка", lst)
-
-
-append_milo()
-print()
-print("атрибуты", append_milo.__defaults__)
-print()
-append_milo()
-print()
-print("атрибуты", append_milo.__defaults__)
+//
